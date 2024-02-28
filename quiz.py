d285c8001b0985e6a173e7562aec6d9942ffcc55
Glenn <glennchiang24@gmail.com>
2024-02-28 11:00:37 +0800

    Add services module which is in charge of making api calls

--- quiz.py
+++ quiz.py
@@ -1,12 +1,23 @@
-from telegram import Update
+from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import CallbackContext, CommandHandler, ConversationHandler, CallbackQueryHandler
 from menu_codes import SELECTING_QUIZ
-import requests
+from services import get_quizzes
+from requests.exceptions import HTTPError
 
 # When the user clicks the 'Select a Quiz' button,
 # show the list of quizzes for the user to select from
 async def show_quizzes(update: Update, callback: CallbackContext):
-    ...
+    update.message.reply_text('Fetching quizzes...')
+    quizzes, error = get_quizzes()
+
+    if error:
+        await update.message.reply_text(f'Error fetching quizzes: {error}')
+        return
+
+    keyboard = []
+
+    await update.message.reply_text(text='Select a quiz from below', reply_markup=ReplyKeyboardMarkup(keyboard))
+
 
 # When the user clicks on a quiz, ...
     
--- services.py
+++ services.py
@@ -1,6 +1,20 @@
+from requests.exceptions import HTTPError, RequestException
+import requests
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
-BASE_URL = os.getenv('BASE_URL') or 'http://localhost:5000'
+API_URL = os.getenv('BASE_URL') or 'http://localhost:5000'
 
+
+def response(data, error_message: str | None = None):
+    return data, error_message
+
+
+def get_quizzes():
+    try:
+        res = requests.get(f'{API_URL}/quizzes')
+        res.raise_for_status()
+        return response(res.json())
+    except RequestException as error:
+        return response(None, error_message=error)
